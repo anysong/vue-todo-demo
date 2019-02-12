37bb2d26e5accf2f0ee2d9dbf420ebb6ca8556a6
dengyingyi2 <dengyingyi@jd.com>
2019-02-12 09:33:07 +0800

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     mode: 'development',
